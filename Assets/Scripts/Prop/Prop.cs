13a19e20fa5a847dad3a359ca43503277fb82057
sittan2 <49196800+sittan2@users.noreply.github.com>
2021-08-18 02:07:43 +0900

    프랍 - 덫 Animation 및 충돌 처리 태그 - Unit, Prop 추가

---
+++ Assets/Scripts/Prop/Prop.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+abstract public class Prop : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpforce = 2f;
+    public bool jumpable = false;
+
+    public virtual void Move(int direction)
+    {
+        Direction dir = (Direction)direction;
+
+        if (dir == Direction.Left)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (dir == Direction.Right)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+
+    public virtual void Jump()
+    {
+        if (jumpable)
+        {
+            transform.DOMoveY(transform.position.y + jumpforce, 0.3f);
+            jumpable = false;
+        }
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        Vector3 normalVector = collision.contacts[0].normal;
+        Debug.Log(normalVector);
+        if (normalVector.y > 0.5)
+        {
+            jumpable = true;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Unit"))
+        {
+            CollisionEnterUnit(collision);
+        }
+
+        if (collision.gameObject.CompareTag("Prop"))
+        {
+            CollisionEnterProp(collision);
+        }
+            
+    }
+
+    public abstract void CollisionEnterUnit(Collision2D collision);
+    public abstract void CollisionEnterProp(Collision2D collision);
+
+}
---
+++ Assets/Scripts/Prop/Thorn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Thorn : Prop
+{
+
+    public override void CollisionEnterUnit(Collision2D collision)
+    {
+        Debug.Log("Call DIE Method Character");
+        GetComponent<Animator>().SetTrigger("Trigger");
+    }
+
+    public override void CollisionEnterProp(Collision2D collision)
+    {
+        Debug.Log("Call Destroy Prop");
+    }
+
+
+    /// <summary>
+    /// Animation Cilp 에서 호출되는 함수
+    /// </summary>
+    public void Destroy()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
