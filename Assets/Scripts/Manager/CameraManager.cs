0f6f806c2804831981e14ee112073d9fb8feaf6d
sittan2 <49196800+sittan2@users.noreply.github.com>
2021-08-21 05:15:19 +0900

    Camera Move By Turn 구현 완료

--- Assets/Scripts/Manager/CameraManager.cs
+++ Assets/Scripts/Manager/CameraManager.cs
@@ -6,21 +6,44 @@ using DG.Tweening;
 public class CameraManager : Singleton<CameraManager>
 {
     Tweener cameraMovingTween;
+    Vector3 initPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initPos = transform.position;
+        cameraMovingTween = transform.DOMove(initPos, 1).SetEase(Ease.OutExpo).SetAutoKill(false);
+        TurnManager.instance.TurnChangeEvent.AddListener(
+            () => NewInitPos(TurnManager.instance.current_player));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void CameraMoving()
+    public void NewInitPos(Player player)
     {
-        
+        initPos = player.transform.position;
+        initPos.z = -1f;
+        CameraMovingByTurn();
+    }
+
+    public void CameraMovingByTurn()
+    {
+        cameraMovingTween.ChangeEndValue(initPos, true).Restart();
+    }
+
+    public void CameraMovingByIcon(bool isMouseEnter)
+    {
+        if (isMouseEnter)
+        {
+
+        }
+        else
+        {
+
+        }
         
     }
 
