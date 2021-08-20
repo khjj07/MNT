578946dbafc028ee6a09f3ab265e898367f0e035
khjj07 <khjj07@gmail.com>
2021-08-20 23:15:50 +0900

    remainturnchangepublic

--- Assets/Scripts/Manager/TurnManager.cs
+++ Assets/Scripts/Manager/TurnManager.cs
@@ -8,7 +8,7 @@ public class TurnManager : Singleton<TurnManager>
     public GameState[] player_list;
     public int TurnLimit = 4;
     private int flag = 0;
-    private int RemainTurn;
+    public int RemainTurn;
     private int player_count;
     public UnityEvent DefeatEvent;
     public UnityEvent EnemyAllDeadEvent;
@@ -16,7 +16,7 @@ public class TurnManager : Singleton<TurnManager>
 
     void Start()
     {
-        RemainTurn = TurnLimit-1;
+        RemainTurn = TurnLimit;
         player_count = player_list.Length;
     }
     public void HostageDead()
