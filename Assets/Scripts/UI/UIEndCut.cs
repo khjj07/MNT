8d25502f2d02b7f3600b3b5a6d8337c19586fa86
sittan2 <49196800+sittan2@users.noreply.github.com>
2021-08-22 07:35:18 +0900

    엔딩 씬 작업 중

---
+++ Assets/Scripts/UI/UIEndCut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class UIEndCut : MonoBehaviour
+{
+    public List<Image> ImageCuts;
+
+    private void Awake()
+    {
+
+    }
+
+    public void StartCut()
+    {
+        ImageCuts[1].GetComponent<RectTransform>()
+        .DOAnchorPosX(-300f, 0.4f).From(true).SetDelay(0.5f);
+
+
+        ImageCuts[1].GetComponent<RectTransform>()
+        .DOAnchorPosX(-300f, 0.4f).From(true).SetDelay(0.5f);
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/UI/UIObjectControllerX.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UniRx;
+using UniRx.Triggers;
+using DG.Tweening;
+
+public class UIObjectControllerX : MonoBehaviour
+{
+    public List<AnimationTarget> targets = new List<AnimationTarget>();
+
+    [System.Serializable]
+    public struct AnimationTarget
+    {
+        public GameObject go;
+        public enum EAnimationType
+        { SlideAndFadeIn = 0 }
+
+
+        //public bool isUpAnimation;
+        public float moveDistance;
+        public float duration;
+        public Ease ease;
+        public float delay;
+
+        public void SlideAndFadeIn()
+        {
+            CanvasGroup canvas;
+
+            if (go.GetComponent<CanvasGroup>())
+                canvas = go.GetComponent<CanvasGroup>();
+            else
+                canvas = go.AddComponent<CanvasGroup>();
+
+            go.transform
+                .DOMoveX(moveDistance, duration)
+                .SetEase(ease)
+                .From(true)
+                .SetRelative(true)
+                .SetDelay(delay);
+
+            canvas
+                .DOFade(1f, duration)
+                .From(0f)
+                .SetDelay(delay);
+        }
+
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        targets.ForEach(x =>
+        {
+            x.SlideAndFadeIn();
+        });
+    }
+
+}
