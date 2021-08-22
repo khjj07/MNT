d29240df1a784261d79c90e60a1cc54fb402aa2f
sittan2 <49196800+sittan2@users.noreply.github.com>
2021-08-22 10:48:58 +0900

    UI 수정, 크레딧 변경, 스테이지 3 추가

---
+++ Assets/Scripts/UI/UILobby.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILobby : MonoBehaviour
+{
+    public bool isReset = false;
+
+    void Start()
+    {
+        if (isReset)
+        {
+            isReset = false;
+            PlayerPrefs.SetInt("1", 0);
+            PlayerPrefs.SetInt("2", 0);
+            PlayerPrefs.SetInt("3", 0);
+            PlayerPrefs.SetInt("4", 0);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
